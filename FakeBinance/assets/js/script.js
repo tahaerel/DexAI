8f557252eb6cd45c8c6c56dffd592571e427eaf1
Taha Erel <tahaerelll@gmail.com>
2023-11-12 02:54:55 +0300

    FakeBinance Added, code optimization, formatting

---
+++ FakeBinance/assets/js/script.js
@@ -0,0 +1,126 @@
+'use strict';
+/**
+ * add event on element
+ */
+
+const ethereumButton = document.querySelector('.btn-outline');
+const showAccount = document.querySelector('.showAccount');
+
+ethereumButton.addEventListener('click', () => {
+  getAccount()
+    .then((account) => {
+      ethereumButton.style.display = 'none'; // Bağlantı butonunu gizle
+      localStorage.setItem('account', account); // Hesap bilgisini yerel depolamada sakla
+      openProfilePage(); // Profil sayfasını aç
+    });
+});
+
+async function getAccount() {
+  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    .catch((err) => {
+      if (err.code === 4001) {
+        console.log('Please connect to MetaMask.');
+      } else {
+        console.error(err);
+      }
+    });
+  const account = accounts[0];
+  showAccount.innerHTML = account;
+  return account;
+}
+
+function openProfilePage() {
+  // Profil sayfasını açmak için tarayıcıyı yönlendirin
+  window.location.href = 'profile.html';
+}
+
+
+
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 300) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeHeader);
+
+
+
+/**
+ * toggle active on add to fav
+ */
+
+const addToFavBtns = document.querySelectorAll("[data-add-to-fav]");
+
+const toggleActive = function () {
+  this.classList.toggle("active");
+}
+
+addEventOnElem(addToFavBtns, "click", toggleActive);
+
+
+//Landbot api Here
+
+
+/**
+ * scroll revreal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1.5) {
+      sections[i].classList.add("active");
+    } else {
+      sections[i].classList.remove("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
